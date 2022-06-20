30606396f9752648810a68b1a69836d17e837016
janco buitendijk <9016333@student.zadkine.nl>
2022-06-20 20:42:56 +0200

    start bot

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+require("dotenv").config();
+
+const TOKEN = "OTg4NDc0MzM4ODA4MzIwMDcw.GDEPj6.rKa7yvsSpd3vzAxeQBhism8-DuZqEpF4ikhqqM";
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+});
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+});
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi bud"){
+        message.reply("hey there buddy!")
+    }
+});
+
+
+
+client.login(process.env.TOKEN)
